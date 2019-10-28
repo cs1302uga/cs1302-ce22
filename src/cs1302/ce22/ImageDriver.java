82c3bd879829048edec28b8a6644d46ff5f3bd3e
Michael Cotterell <mepcotterell@gmail.com>
2019-10-28 09:12:54 -0400

    Update ImageDriver.java

--- src/cs1302/ce22/ImageDriver.java
+++ src/cs1302/ce22/ImageDriver.java
@@ -2,6 +2,9 @@ package cs1302.ce22;
 
 import javafx.application.Application;
 
+/**
+ * Driver for image application.
+ */
 public class ImageDriver {
 
     public static void main(String[] args) {
@@ -9,6 +12,7 @@ public class ImageDriver {
             Application.launch(ImageApp.class, args);
         } catch (Exception e) {
             System.err.println(e);
+            e.printStackTrace();
             System.err.println("Likely due to X11 timeout. Logout and log back in...");
             System.exit(1);
         } // try
